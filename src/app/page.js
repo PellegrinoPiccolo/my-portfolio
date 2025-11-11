f845e16066c888bcf86f016dd2dd3114ce777075
Pellegrino Piccolo <piccolopellegrino22@gmail.com>
2025-11-11 19:18:32 +0100

    add header and hero components; update package.json and globals.css for new dependencies

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
+import Header from "../../components/Header";
+import Hero from "../../components/Hero";
+
 export default function Page() {
   return (
     <div>
+      {/* <Header /> */}
+      <Hero />
     </div>
   );
 }
