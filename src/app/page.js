e26d055d2417783c7de9b467a28d5b0eafec2044
Pellegrino Piccolo <piccolopellegrino22@gmail.com>
2025-11-12 18:42:19 +0100

    add skills section with detailed technology and skill categories for enhanced portfolio presentation

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import About from "../../components/sections/About";
 import Header from "../../components/sections/Header";
 import Hero from "../../components/sections/Hero";
 import Projects from "../../components/sections/Projects";
+import Skills from "../../components/sections/Skills";
 
 export default function Page() {
   return (
@@ -10,6 +11,7 @@ export default function Page() {
       <Hero />
       <About />
       <Projects />
+      <Skills />
     </div>
   );
 }
