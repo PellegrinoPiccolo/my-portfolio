4fef6f155a8e0d57c6b6e9bdcdd9a026e663dd3a
Pellegrino Piccolo <piccolopellegrino22@gmail.com>
2026-07-28 19:57:12 +0200

    feat: enhance header layout and improve button accessibility on projectspage

--- components/sections/ProjectsPage.jsx
+++ components/sections/ProjectsPage.jsx
@@ -29,14 +29,16 @@ const ProjectsPage = () => {
 
   return (
     <div className='w-full mx-auto px-8 py-30 min-h-screen bg-background-secondary scroll-mt-20'>
-        <div className={`fixed top-0 left-0 w-full py-2 px-4 z-999 flex gap-3 items-center border-gray-800 ${scrolled ? 'bg-gray-950/95 backdrop-blur-sm border-b' : 'bg-transparent'} transition-colors duration-300`}>
-            <button onClick={() => router.push('/')} className='group text-white hover:bg-gray-700 p-2 rounded-full transition cursor-pointer'>
-                <GoHome className="text-2xl group-hover:hidden" />
-                <GoHomeFill className="text-2xl hidden group-hover:block" />
-            </button>
-            <button onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className='cursor-pointer hover:scale-105 transition-transform duration-300'>
-                <img src="/logo.png" alt="Logo" className='h-8' />
-            </button>
+        <div className={`fixed top-0 left-0 w-full py-4 px-8 z-999 flex justify-between items-center border-gray-800 ${scrolled ? 'bg-gray-950/95 backdrop-blur-sm border-b' : 'bg-transparent'} transition-colors duration-300`}>
+            <div className='max-w-7xl mx-auto w-full flex justify-between items-center py-3.5'>
+                <button onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className='cursor-pointer hover:scale-105 transition-transform duration-300'>
+                    <img src="/logo.png" alt="Logo" className='h-8' />
+                </button>
+                <button title="Go to Home" onClick={() => router.push('/')} className='group text-white hover:bg-gray-700 p-2 rounded-full transition cursor-pointer'>
+                    <GoHome size={24} className="group-hover:hidden" />
+                    <GoHomeFill size={24} className="hidden group-hover:block" />
+                </button>
+            </div>
         </div>
         <motion.h2 className="md:text-5xl font-light mb-6 text-center text-white text-3xl" initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>Featured Projects</motion.h2>
         <motion.div className='w-20 h-1 bg-linear-to-r from-indigo-400 to-purple-500 mx-auto rounded-2xl' initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}></motion.div>
